9de94a2b6866e6b829e5d9e503282969acd15d25
chittiapp <131455579+chittiapp@users.noreply.github.com>
2023-12-19 12:12:10 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,11 +1,10 @@
 sudo apt-get update -y
 sudo apt install -y ubuntu-gnome-desktop
 
-sudo apt install -y xserver-xorg-core
-sudo apt install -y tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
-
+sudo apt-get install -y ubuntu-restricted-extras
 
 sudo systemctl start gdm
 sudo systemctl enable gdm
 
-sudo apt-get install -y ubuntu-restricted-extras
+sudo apt install -y xserver-xorg-core
+sudo apt install -y tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
